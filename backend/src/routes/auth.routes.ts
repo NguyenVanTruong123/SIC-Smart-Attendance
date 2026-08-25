1671d76c22ebae509b9acdcc79df55d480144394
NguyenVanTruong <truongzz12345zz@gmail.com>
2026-08-25 23:03:07 +0700

    SSAS-39: implement auth and RBAC middlewares and getMe profile route

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { authController } from '../controllers/auth.controller';
+import { verifyToken } from '../middlewares/auth.middlewares'; 
+
 
 const router = Router();
 
@@ -9,5 +11,8 @@ const router = Router();
  * @access  Public
  */
 router.post('/login', (req, res, next) => authController.login(req, res, next));
+
+// Route Lấy thông tin cá nhân (Được bảo vệ - Bắt buộc phải có Token JWT hợp lệ)
+router.get('/me', verifyToken, (req, res, next) => authController.getMe(req, res, next));
 
 export default router;
